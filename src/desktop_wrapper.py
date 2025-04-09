1f307f471d2850774e6b4ac67286bab2fc5ee2e5
Kay0n <kayon5555@gmail.com>
2025-04-09 15:33:00 +0930

    adding mutex lock ensuring single instance of app

--- src/desktop_wrapper.py
+++ src/desktop_wrapper.py
@@ -1,8 +1,28 @@
 import webview
+import fasteners
 import logging
 import sys
+import atexit
 
 from app import flask_app 
+
+
+
+def setup_instance_lock(flask_app):
+    lock_path = flask_app.config.get('APP_LOCK_FILE') 
+
+    lock = fasteners.InterProcessLock(lock_path)
+    lock_aquired = lock.acquire(blocking=False)
+
+    if not lock_aquired:
+        logging.error(f"Another instance of {flask_app.config.get('APP_NAME')} is already running.")
+        sys.exit(0)
+
+    def release_lock():
+        logging.info("Releasing application lock.")
+        lock.release()
+
+    atexit.register(release_lock) 
 
 
 def run_desktop_app(flask_app) -> webview.Window:
@@ -25,8 +45,11 @@ def run_desktop_app(flask_app) -> webview.Window:
 
 
 
+
+setup_instance_lock(flask_app)
 run_desktop_app(flask_app)
 
-logging.info("Application window closed.")
+
+logging.info("Application shutting down")
 logging.shutdown()
 
